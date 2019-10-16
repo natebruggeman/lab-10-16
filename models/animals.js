35f16a30ca6de0dd8af9e2dc0f0da42200af5cb8
Nate Bruggeman <you@example.com>
2019-10-16 15:17:55 -0500

    animal data created

---
+++ models/animals.js
@@ -0,0 +1,24 @@
+const animals = [
+{
+	name: 'dog',
+	color: 'black',
+	tail: true
+},
+{
+	name: 'cat',
+	color: 'orange',
+	tail: true
+},
+{
+	name: 'pig',
+	color: 'pink',
+	tail: true
+},
+{
+	name: 'crab',
+	color: 'red',
+	tail: false
+},
+];
+
+module.exports = animals
