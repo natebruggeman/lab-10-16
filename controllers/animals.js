f2551d7993317f32bb83fd751eec9ef74306e8f6
Nate Bruggeman <you@example.com>
2019-10-16 17:03:53 -0500

    morning portion complete

--- controllers/animals.js
+++ controllers/animals.js
@@ -4,13 +4,36 @@ const Animals = require('../models/animals')
 
 
 
-
+console.log("we are inside animalsController")
 router.get('/', (req, res) => {
 	res.render('index.ejs', {
 		animals: Animals
 	})
 })
 
+
+router.post('/', (req, res) => {
+	// Animals.push(req.body);
+	res.redirect('/animals')
+})
+
+
+// router.put('/:index', (req, res) => {
+	
+// 	Animals[req.params.index] = req.body;
+
+// 	res.redirect('/animals')
+
+// })
+
+
+
+router.delete('/:index', (req, res) => {
+	Animals.splice(req.params.index, 1);
+	console.log("hitting delete route")
+	res.redirect('/animals');
+
+})
 
 
 
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 const methodOverride = require('method-override')
+const bodyParser = require('body-parser');
 
 
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
 
@@ -11,7 +14,7 @@ app.use(methodOverride('_method'));
 const animalController = require('./controllers/animals')
 
 
-
+console.log("about to use animalsController");
 app.use('/animals', animalController);
 
 
